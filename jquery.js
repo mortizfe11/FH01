7b99d0a1817c6abcd8bae3fdffa8c76219c8743f
MARTA ORTIZ FERNANDEZ <mortizfe11.alumnes@ub052229.mat.ub.edu>
2016-05-11 17:23:25 +0200

    init

---
+++ jquery.js
@@ -0,0 +1,17 @@
+//funcion para menú desplegable
+var main = function() {
+  $(".dropdown-toggle").click(function() {
+    $(".dropdown-menu").show();
+  });
+};
+
+$(document).ready(main);
+
+//Button click para el calculo
+$( "#calculo" ).click(function() {
+
+}
+//Button click para los consejos
+$( "#consejos" ).click(function() {
+
+}
